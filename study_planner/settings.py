d5b61cdd195d663054390956945b7583e467ee0c
Raj Popatni <rajpopatani46@gmail.com>
2026-06-10 12:24:45 +0530

    Fix static files warning

--- study_planner/settings.py
+++ study_planner/settings.py
@@ -61,7 +61,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
